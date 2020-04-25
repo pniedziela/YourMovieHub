9a6c39a1c1d40be750b98425353efeaedd0a4b6c
labudziak23 <labuda.michal23@gmail.com>
2020-04-25 12:57:50 +0200

    auth in vue

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -26,10 +26,10 @@ signupForm.addEventListener('submit', (e)=>{
 
 //logout 
 const logout = document.querySelector('#logout');
-logout.addEventListener('click', (e) => {
-    e.preventDefault();
-    auth.signOut();
-});
+// logout.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     auth.signOut();
+// });
 
 
 //login
